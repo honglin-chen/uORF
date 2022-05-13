a310e8b55d090efd6898ea42b0b849d4fd6f2e7d
Wanhee Lee <wanhee@stanford.edu>
2022-05-12 22:09:35 -0700

    before EISEN segment test

---
+++ scripts/18006.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DATAROOT=${1:-'tdw_30obj_multibg'}
+PORT=${2:-18006}
+NSCENES=${3:-3000}
+python -m visdom.server -p $PORT &>/dev/null &
+python train.py --dataroot $DATAROOT --n_scenes $NSCENES --n_img_each_scene 4  \
+    --checkpoints_dir 'checkpoints' --name '18006' \
+    --display_port $PORT --display_ncols 4 --print_freq 200 --display_freq 200 --display_grad \
+    --load_size 128 --n_samp 128 --input_size 128 --mask_size 128 --supervision_size 64 \
+    --niter 400 --coarse_epoch 200 --z_dim 64 --num_slots 4 \
+    --save_latest_freq 500 \
+    --model 'uorf_train' \
+    --focal_ratio 0.9605 0.9605 \
+    --near_plane 1 --far_plane 8 \
+    --unified_decoder \
+    --no_locality_epoch 0 \
+    --gt_seg \
+    --pixel_encoder --mask_image \
+    --use_ray_dir \
+    --restrict_world \
+    --frame5 \
+    --bg_no_pixel \
+
+echo "Done"
+# node5 cuda 5
