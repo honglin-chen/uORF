d6adafbb82047aa9c61add3f79b81f31628a080a
Wanhee Lee <wanhee@stanford.edu>
2022-06-06 22:49:06 -0700

    running experiment debug_refactor_tdw.sh

--- scripts/debug_refactor_tdw.sh
+++ scripts/debug_refactor_tdw.sh
@@ -2,9 +2,9 @@
 DATAROOT=${1:-'tdw_20obj_2000'}
 PORT=${2:-8011}
 python -m visdom.server -p $PORT &>/dev/null &
-python train.py --dataroot $DATAROOT --n_scenes 16 --n_img_each_scene 4  \
+python train.py --dataroot $DATAROOT --n_scenes 1000 --n_img_each_scene 4  \
     --checkpoints_dir 'checkpoints' --name 'debug' \
-    --display_port $PORT --display_ncols 4 --print_freq 16 --display_freq 16 --display_grad \
+    --display_port $PORT --display_ncols 4 --print_freq 200 --display_freq 200 --display_grad \
     --load_size 128 --n_samp 64 --input_size 128 --supervision_size 64 --batch_size 4 \
     --niter 1200 --coarse_epoch 600 --z_dim 64 --num_slots 4 \
     --save_latest_freq 500 \
@@ -13,6 +13,7 @@ python train.py --dataroot $DATAROOT --n_scenes 16 --n_img_each_scene 4  \
     --percept_in 100 \
     --focal_ratio 0.9605 0.9605 \
     --near_plane 1 --far_plane 15 \
-    --use_slot_feat --use_pixel_feat --gt_seg --frame5 \
+    --use_slot_feat --use_pixel_feat --gt_seg --frame5 --mask_image \
+    --continue_train --exp_id 'latest' \
 # done
 echo "Done"
